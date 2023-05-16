beef73bd201ca8c5c2dcc5f8d3f7b98dcf48e9fb
Harinder <96901742+harinder007@users.noreply.github.com>
2023-05-16 21:43:43 +0530

    Add files via upload

---
+++ example1.js
@@ -0,0 +1,15 @@
+var Employee = /** @class */ (function () {
+    function Employee() {
+    }
+    Employee.prototype.display = function () {
+        console.log("employee name" + this.empName + "employee id " + this.empId);
+    };
+    Employee.prototype.setName = function (str) {
+        this.empName = str;
+    };
+    return Employee;
+}());
+var emp1 = new Employee();
+emp1.setName('Harinder');
+emp1.empId = 567;
+emp1.display();
