dbbbd468409444c5d9722440dcabe457253334c1
Harinder <96901742+harinder007@users.noreply.github.com>
2023-05-16 21:44:04 +0530

    Add files via upload

---
+++ exaample2.js
@@ -0,0 +1,8 @@
+function fullName(fName, lName) {
+    if (lName === void 0) { lName = "Yadav"; }
+    return fName + " " + lName;
+}
+console.log(fullName('Harinder'));
+//arrow function 
+var print1 = function () { return console.log("Welcome in the typescript"); };
+print1();
